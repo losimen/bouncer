c6251ccfb938a614341753ac62668e9fa43a9c03
losimen <koval.pavlo03@gmail.com>
2024-05-04 16:54:26 +0300

    ghosts moving

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const CANVAS = document.getElementById('myCanvas')
 const BALL_RADIUS = 69
-const BALL_SPEED = 1000
+const BALL_SPEED = 300
 const MAIN_BALL_INDEX = 0
 
 class Game {
@@ -21,7 +21,7 @@ class V2 {
     }
 
     sub (oth) {
-        return new V2(this.x + oth.x, this.y + oth.y)
+        return new V2(this.x - oth.x, this.y - oth.y)
     }
 
     scale (s) {
@@ -34,9 +34,28 @@ class V2 {
             Math.sin(angle) * this.x + Math.cos(angle) * this.y,
         )
     }
+
+    length() {
+        return Math.sqrt(this.x ** 2 + this.y ** 2);
+    }
+
+    normalize() {
+        const len = this.length();
+        if (len === 0) {
+            return new V2(0, 0);  // to avoid division by zero
+        }
+        return new V2(this.x / len, this.y / len);
+    }
+
+    lerp(oth, t) {
+        return new V2(
+            this.x + t * (oth.x - this.x),
+            this.y + t * (oth.y - this.y)
+        );
+    }
 }
 
-class Ball {
+class MainBall {
     constructor (context, center, radius, velocity, color = "green") {
         this.context = context
         this.center = center
@@ -55,6 +74,41 @@ class Ball {
     update (dt) {
         const displacement = this.velocity.scale(dt)
         this.center = this.center.add(displacement)
+    }
+}
+
+
+class GhostBall {
+    constructor (context, center, radius, velocity, ghostIndex, color = "green") {
+        this.context = context
+        this.center = center
+        this.radius = radius
+        this.velocity = velocity
+        this.color = color
+        this.ghostIndex = ghostIndex
+    }
+
+    draw() {
+        this.context.beginPath();
+        this.context.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI, false);
+        this.context.fillStyle = this.color;
+        this.context.fill();
+    }
+
+    update(dt) {
+        console.log(this.ghostIndex, this.color, )
+        const followingBall = game.entities[this.ghostIndex]
+
+        if (this.ghostIndex === 0) {
+            this.velocity = new V2(-followingBall.velocity.x, -followingBall.velocity.y)
+        } else {
+            this.velocity = new V2(followingBall.velocity.x, followingBall.velocity.y)
+        }
+        this.velocity = this.velocity.normalize()
+        this.center = new V2(
+            followingBall.center.x + this.velocity.x * 10,
+            followingBall.center.y + this.velocity.y * 10 
+        )
     }
 }
 
@@ -110,10 +164,9 @@ function update (context, dt) {
     }
 
     for (const entity of game.entities) {
-        const didWork = borderService(entity)
-        // if (didWork && entity === game.entities[MAIN_BALL_INDEX]) {
-        //     console.log('do this')
-        // }
+        if (entity === game.entities[MAIN_BALL_INDEX]) {
+            const didWork = borderService(entity)
+        }
     }
 
     for (const entity of game.entities) {
@@ -125,7 +178,13 @@ function main () {
     setCanvasSize();
 
     const context = CANVAS.getContext("2d")
-    game.entities.push(new Ball(context, new V2(100, 100), 10, new V2(0, BALL_SPEED)))
+    game.entities.push(new MainBall(context, new V2(100, 100), 10, new V2(0, BALL_SPEED)))
+    game.entities.push(new GhostBall(context, new V2(100, 100), 10, new V2(0, 0), 0, 'red'))
+    game.entities.push(new GhostBall(context, new V2(100, 100), 10, new V2(0, 0), 1, 'blue'))
+    game.entities.push(new GhostBall(context, new V2(100, 100), 10, new V2(0, 0), 2, 'blue'))
+    game.entities.push(new GhostBall(context, new V2(100, 100), 10, new V2(0, 0), 3, 'blue'))
+    game.entities.push(new GhostBall(context, new V2(100, 100), 10, new V2(0, 0), 4, 'blue'))
+
     let start
 
     function gameLoop (timestamp) {
